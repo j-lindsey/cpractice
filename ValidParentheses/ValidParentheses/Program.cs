8a931bd7a9b8dd17fbcbb54114f3ad26d0444d88
j-lindsey <j-lindsey@users.noreply.github.com>
2020-11-18 15:27:11 -0700

    validParentheses

---
+++ ValidParentheses/ValidParentheses/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        public static bool IsValid(string s)
+        {
+            List<char> stack = new List<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                {
+                    stack.Add(s[i]);
+                }
+                else if (stack.Count == 0)
+                {
+                    return false;
+                }
+                else
+                {
+
+                    if (s[i] == ')')
+                    {
+                        if (stack[stack.Count - 1] == '(')
+                        {
+                            stack.RemoveAt(stack.Count - 1);
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else if (s[i] == '}')
+                    {
+                        if (stack[stack.Count - 1] == '{')
+                        {
+                            stack.RemoveAt(stack.Count - 1);
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else if (s[i] == ']')
+                    {
+                        if (stack[stack.Count - 1] == '[')
+                        {
+                            stack.RemoveAt(stack.Count - 1);
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+
+                }
+            }
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string s = "()";
+            bool result = IsValid(s);
+            Console.WriteLine(s + "  :" + result);
+            s = "()[]{}";
+            result = IsValid(s);
+            Console.WriteLine(s + "  :" + result);
+            s = "(]";
+            result = IsValid(s);
+            Console.WriteLine(s + "  :" + result);
+            s = "(])]";
+            result = IsValid(s);
+            Console.WriteLine(s + "  :" + result);
+            s = "{[]}";
+            result = IsValid(s);
+            Console.WriteLine(s + "  :" + result);
+            Console.ReadLine();
+        }
+    }
+}
